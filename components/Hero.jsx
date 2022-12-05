5afb9d7c74154e618ce7061d4107cd4dcd1fa2a2
ImmortalDev <34840787+SikanaAli@users.noreply.github.com>
2022-12-05 22:08:16 +0200

    hero section

---
+++ components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='w-full h-screen text-center'>
+        <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+            <div>
+                <p className='uppercase text-sm tracking-widest'>let&apos;s build something together</p>
+                <h1 className='py-4 text-gray-700'>Hi, I&apos;m <span className='text-[#5651e5]'>Alinaswe</span></h1>
+                <h1 className='py-2 text-gray-700'>A Softaware Engineer</h1>
+                <p>
+                    I&apos;m a software engineer specializing in back-end development, Internet of Things (IoT), 
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
