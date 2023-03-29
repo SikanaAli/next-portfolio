19655584ef9c734697e7c8ae3f0eb3f52f892e7a
ImmortalDev <34840787+SikanaAli@users.noreply.github.com>
2023-03-29 05:55:49 +0200

    added bg to hero

--- components/About.jsx
+++ components/About.jsx
@@ -3,52 +3,113 @@ import React from "react";
 import Link from "next/link";
 
 export default function About() {
-  return (
-    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
-      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
-        <div className="col-span-2">
-          <p className="uppercase text-xl tracking-widest text-[#5651e5]">
-            About
-          </p>
-          <h2 className="py-4">Who I Am</h2>
-          <p className="py-2 text-gray-600">
-            {"// I am not you regular developer"}
-          </p>
-          <p className="py-2 text-gray-600">
-            Growing up I had always enjoyed playing videogames; the more I
-            played the more I wanted to know how the very games I played where
-            made. While pursuing my Diploma in IT an opportunity arose in which
-            I had decide what my Final Year Project would be. I proceeded to
-            develop a game using Unity Game Engine alongside the Thesis (late
-            2016).
-          </p>
-          <p className="py-2 text-gray-600">
-            Following the success of my Diploma Project, I pursued a Degree in
-            Software Engineering. Which saw me produce an Internet of Things
-            (IoT) project for my thesis. Shortly after, I found a 5-month
-            internship opportunity with Custommedia Snd Bhd Malaysia (Feb,
-            2019). In the 5 months there, my knowledge of C-Sharp and other
-            technologies grew which then helped me get employed as an Analyst
-            Programmer by said company till late 2021. Fascinated with the
-            intricacies of programming and software engineering, I was quickly
-            drawn to learn more. In addition to C-Sharp, I started learning
-            JavaScript for Web, CircuitPython for Micro-Controllers as well as
-            Postgres-SQL along side MSSQL.
-          </p>
-          <p className="py-2 text-gray-600">
-            I now spend my time Building projects with NextJS, Superbase and
-            learning new technologies
-          </p>
-          <a href="#projects">
-            <p className="py-2 text-gray-600 underline cursor-pointer">
-              check out some of my latest projects
-            </p>
-          </a>
-        </div>
-        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-          <img className="rounded-xl" src="/assets/profile_img.jpg" alt="" />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			id="about"
+			className="w-full md:h-screen p-2 flex items-center py-16"
+		>
+			<div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+				<svg
+					class="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+					viewBox="0 0 1155 678"
+				>
+					<path
+						fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+						fill-opacity=".8"
+						d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+					/>
+					<defs>
+						<linearGradient
+							id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+							x1="1155.49"
+							x2="-78.208"
+							y1=".177"
+							y2="474.645"
+							gradientUnits="userSpaceOnUse"
+						>
+							<stop stop-color="#9089FC" />
+							<stop offset="1" stop-color="#FF80B5" />
+						</linearGradient>
+					</defs>
+				</svg>
+			</div>
+
+			<div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+				<div className="col-span-2">
+					<p className="uppercase text-xl tracking-widest text-[#5651e5]">
+						About
+					</p>
+					<h2 className="py-4">Who I Am</h2>
+					<p className="py-2 text-gray-600">
+						{"// I am not you regular developer"}
+					</p>
+					<p className="py-2 text-gray-600">
+						Growing up I had always enjoyed playing videogames; the
+						more I played the more I wanted to know how the very
+						games I played where made. While pursuing my Diploma in
+						IT an opportunity arose in which I had decide what my
+						Final Year Project would be. I proceeded to develop a
+						game using Unity Game Engine alongside the Thesis (late
+						2016).
+					</p>
+					<p className="py-2 text-gray-600">
+						Following the success of my Diploma Project, I pursued a
+						Degree in Software Engineering. Which saw me produce an
+						Internet of Things (IoT) project for my thesis. Shortly
+						after, I found a 5-month internship opportunity with
+						Custommedia Snd Bhd Malaysia (Feb, 2019). In the 5
+						months there, my knowledge of C-Sharp and other
+						technologies grew which then helped me get employed as
+						an Analyst Programmer by said company till late 2021.
+						Fascinated with the intricacies of programming and
+						software engineering, I was quickly drawn to learn more.
+						In addition to C-Sharp, I started learning JavaScript
+						for Web, CircuitPython for Micro-Controllers as well as
+						Postgres-SQL along side MSSQL.
+					</p>
+					<p className="py-2 text-gray-600">
+						I now spend my time Building projects with NextJS,
+						Superbase and learning new technologies
+					</p>
+					<a href="#projects">
+						<p className="py-2 text-gray-600 underline cursor-pointer">
+							check out some of my latest projects
+						</p>
+					</a>
+				</div>
+				<div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+					<img
+						className="rounded-xl"
+						src="/assets/profile_img.jpg"
+						alt=""
+					/>
+				</div>
+			</div>
+			<div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+				<svg
+					class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+					viewBox="0 0 1155 678"
+				>
+					<path
+						fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+						fill-opacity=".8"
+						d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+					/>
+					<defs>
+						<linearGradient
+							id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
+							x1="1155.49"
+							x2="-78.208"
+							y1=".177"
+							y2="474.645"
+							gradientUnits="userSpaceOnUse"
+						>
+							<stop stop-color="#9089FC" />
+							<stop offset="1" stop-color="#FF80B5" />
+						</linearGradient>
+					</defs>
+				</svg>
+			</div>
+		</div>
+	);
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,15 +8,18 @@ import "./Hero.module.css";
 export default function Hero() {
 	return (
 		<div id="home" className="w-full h-screen text-center home">
-			{/* <style jsx>
+			<style jsx>
 				{`
 					#home {
-						background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='100%25' width='100%25'%3E%3Cdefs%3E%3Cpattern id='doodad' width='40' height='40' viewBox='0 0 40 40' patternUnits='userSpaceOnUse' patternTransform='rotate(221)'%3E%3Crect width='100%25' height='100%25' fill='rgba(240, 255, 244,1)'/%3E%3Ccircle cx='20' cy='20' r='1' fill='rgba(107, 70, 193,1)'/%3E%3Ccircle cx='30' cy='20' r='1' fill='rgba(246, 224, 94,1)'/%3E%3Ccircle cx='25' cy='28.66' r='1' fill='rgba(246, 224, 94,1)'/%3E%3Ccircle cx='15' cy='28.66' r='1' fill='rgba(246, 224, 94,1)'/%3E%3Ccircle cx='10' cy='20' r='1' fill='rgba(246, 224, 94,1)'/%3E%3Ccircle cx='15' cy='11.34' r='1' fill='rgba(246, 224, 94,1)'/%3E%3Ccircle cx='25' cy='11.34' r='1' fill='rgba(246, 224, 94,1)'/%3E%3Ccircle cx='30' cy='20' r='1' fill='rgba(246, 224, 94,1)'/%3E%3C/pattern%3E%3C/defs%3E%3Crect fill='url(%23doodad)' height='200%25' width='200%25'/%3E%3C/svg%3E ");
+						background-color: #EEDBDF;
+background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 1500'%3E%3Cdefs%3E%3CradialGradient id='a' gradientUnits='objectBoundingBox'%3E%3Cstop offset='0' stop-color='%23FFFFFF'/%3E%3Cstop offset='1' stop-color='%23EEDBDF'/%3E%3C/radialGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='0' y1='750' x2='1550' y2='750'%3E%3Cstop offset='0' stop-color='%23f7edef'/%3E%3Cstop offset='1' stop-color='%23EEDBDF'/%3E%3C/linearGradient%3E%3Cpath id='s' fill='url(%23b)' d='M1549.2 51.6c-5.4 99.1-20.2 197.6-44.2 293.6c-24.1 96-57.4 189.4-99.3 278.6c-41.9 89.2-92.4 174.1-150.3 253.3c-58 79.2-123.4 152.6-195.1 219c-71.7 66.4-149.6 125.8-232.2 177.2c-82.7 51.4-170.1 94.7-260.7 129.1c-90.6 34.4-184.4 60-279.5 76.3C192.6 1495 96.1 1502 0 1500c96.1-2.1 191.8-13.3 285.4-33.6c93.6-20.2 185-49.5 272.5-87.2c87.6-37.7 171.3-83.8 249.6-137.3c78.4-53.5 151.5-114.5 217.9-181.7c66.5-67.2 126.4-140.7 178.6-218.9c52.3-78.3 96.9-161.4 133-247.9c36.1-86.5 63.8-176.2 82.6-267.6c18.8-91.4 28.6-184.4 29.6-277.4c0.3-27.6 23.2-48.7 50.8-48.4s49.5 21.8 49.2 49.5c0 0.7 0 1.3-0.1 2L1549.2 51.6z'/%3E%3Cg id='g'%3E%3Cuse href='%23s' transform='scale(0.12) rotate(60)'/%3E%3Cuse href='%23s' transform='scale(0.2) rotate(10)'/%3E%3Cuse href='%23s' transform='scale(0.25) rotate(40)'/%3E%3Cuse href='%23s' transform='scale(0.3) rotate(-20)'/%3E%3Cuse href='%23s' transform='scale(0.4) rotate(-30)'/%3E%3Cuse href='%23s' transform='scale(0.5) rotate(20)'/%3E%3Cuse href='%23s' transform='scale(0.6) rotate(60)'/%3E%3Cuse href='%23s' transform='scale(0.7) rotate(10)'/%3E%3Cuse href='%23s' transform='scale(0.835) rotate(-40)'/%3E%3Cuse href='%23s' transform='scale(0.9) rotate(40)'/%3E%3Cuse href='%23s' transform='scale(1.05) rotate(25)'/%3E%3Cuse href='%23s' transform='scale(1.2) rotate(8)'/%3E%3Cuse href='%23s' transform='scale(1.333) rotate(-60)'/%3E%3Cuse href='%23s' transform='scale(1.45) rotate(-30)'/%3E%3Cuse href='%23s' transform='scale(1.6) rotate(10)'/%3E%3C/g%3E%3C/defs%3E%3Cg transform='translate(900 0)'%3E%3Cg transform='translate(0 150)'%3E%3Ccircle fill='url(%23a)' r='3000'/%3E%3Cg opacity='0.5'%3E%3Ccircle fill='url(%23a)' r='2000'/%3E%3Ccircle fill='url(%23a)' r='1800'/%3E%3Ccircle fill='url(%23a)' r='1700'/%3E%3Ccircle fill='url(%23a)' r='1651'/%3E%3Ccircle fill='url(%23a)' r='1450'/%3E%3Ccircle fill='url(%23a)' r='1250'/%3E%3Ccircle fill='url(%23a)' r='1175'/%3E%3Ccircle fill='url(%23a)' r='900'/%3E%3Ccircle fill='url(%23a)' r='750'/%3E%3Ccircle fill='url(%23a)' r='500'/%3E%3Ccircle fill='url(%23a)' r='380'/%3E%3Ccircle fill='url(%23a)' r='250'/%3E%3C/g%3E%3Cg transform='rotate(-190.8 0 0)'%3E%3Cuse href='%23g' transform='rotate(10)'/%3E%3Cuse href='%23g' transform='rotate(120)'/%3E%3Cuse href='%23g' transform='rotate(240)'/%3E%3C/g%3E%3Ccircle fill-opacity='0' fill='url(%23a)' r='3000'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+background-attachment: fixed;
+background-size: cover;
 					}
 				`}
-			</style> */}
+			</style>
 			<div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-				<div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+				{/* <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
 					<svg
 						class="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
 						viewBox="0 0 1155 678"
@@ -40,7 +43,7 @@ export default function Hero() {
 							</linearGradient>
 						</defs>
 					</svg>
-				</div>
+				</div> */}
 				<div>
 					<p className="uppercase text-sm tracking-widest">
 						let&apos;s build something together
@@ -70,31 +73,7 @@ export default function Hero() {
 					</div>
 				</div>
 			</div>
-			<div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-				<svg
-					class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
-					viewBox="0 0 1155 678"
-				>
-					<path
-						fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
-						fill-opacity=".3"
-						d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-					/>
-					<defs>
-						<linearGradient
-							id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
-							x1="1155.49"
-							x2="-78.208"
-							y1=".177"
-							y2="474.645"
-							gradientUnits="userSpaceOnUse"
-						>
-							<stop stop-color="#9089FC" />
-							<stop offset="1" stop-color="#FF80B5" />
-						</linearGradient>
-					</defs>
-				</svg>
-			</div>
+		
 		</div>
 	);
 }
