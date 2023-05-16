f9f71fe42d598e42ffb4acd6a0d57019a8d795d9
ImmortalDev <34840787+SikanaAli@users.noreply.github.com>
2023-05-16 12:23:56 +0200

    fixed g-analytics error 1

--- app/layout.js
+++ app/layout.js
@@ -4,15 +4,6 @@ export default function RootLayout({ children }) {
 		<html>
 			<head />
 			<body className=" relative">{children}</body>
-			<script
-				async
-				src="https://www.googletagmanager.com/gtag/js?id=G-D23MP7G9SR"
-			></script>
-			<script>
-				window.dataLayer = window.dataLayer || []; function gtag()
-				{dataLayer.push(arguments)}
-				gtag('js', new Date()); gtag('config', 'G-D23MP7G9SR')
-			</script>
 		</html>
 	);
 }
