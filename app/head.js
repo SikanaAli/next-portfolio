86b1a73520dc93dd06d080cfba1119b8562a7f66
ImmortalDev <34840787+SikanaAli@users.noreply.github.com>
2023-05-16 14:34:59 +0200

    analytice key now relfecting

--- app/head.js
+++ app/head.js
@@ -16,7 +16,7 @@ export default function Head() {
 			<Script strategy="lazyOnload">
 				{`window.dataLayer = window.dataLayer || []; function gtag()
 				{dataLayer.push(arguments)}
-				gtag('js', new Date()); gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS})`}
+				gtag('js', new Date()); gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}')`}
 			</Script>
 		</>
 	);
